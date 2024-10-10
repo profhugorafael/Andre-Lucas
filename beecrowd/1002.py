58f31cf5f2325e1c3963cca9a8b3ee2670ee3404
hgrafa <hugorafell@gmail.com>
2024-10-09 22:28:49 -0300

    updating notes

---
+++ beecrowd/1002.py
@@ -0,0 +1,7 @@
+raio = float(input())
+PI = 3.14159
+
+# area = PI * raio * raio
+area = PI * (raio ** 2)
+
+print(f'A={area:.4f}')
---
+++ beecrowd/1010.py
@@ -0,0 +1,7 @@
+linha = input()
+
+lista = linha.split()
+
+a = int(lista[0])
+b = int(lista[1])
+c = int(lista[2])
